c71a45cffc7f6317fc24001748ac22adb18d226a
nguyenhoangnhan2003 <140692347+nguyenhoangnhan2003@users.noreply.github.com>
2024-02-27 21:56:11 +0700

    27/02/2024

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import configViewEngine from './configs/viewEngine';
+import initWebRoute from './routes/web';
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 8081;
+
+//config view engine
+configViewEngine(app);
+
+//init web route
+initWebRoute(app);
+
+app.listen(PORT, () => {
+    console.log(`>>> Web is running on the port: localhost:${PORT}`);
+})
